882248b56cbe6da2ba97b2553f17da2e17e2b904
Keegan Wilson <keegan.wilson@gmail.com>
2022-05-25 13:47:07 -0600

    through app-4

--- app-4/src/App.js
+++ app-4/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const usernameChangeHandler = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const clickHandler = (event) => {
+    alert(`Username: ${username} Password: ${password}`);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input className="login" onChange={usernameChangeHandler}></input>
+      <input className="password" onChange={passwordChangeHandler}></input>
+      <button onClick={clickHandler}>Submit</button>
     </div>
   );
-}
+};
 
 export default App;
