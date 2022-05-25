4b69ade7b5d8f2b7f67ab0eb0b34c1ad757ba32c
Keegan Wilson <keegan.wilson@gmail.com>
2022-05-25 14:06:33 -0600

    through app-5

---
+++ app-5/src/Image.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+const Image = (props) => {
+    return (
+        <div>
+            <img src={props.image}/>
+        </div>
+    )
+}
+
+export default Image
