146bbda8f28a261e4cd0f08e6d19dff9ae7b56c2
Keegan Wilson <keegan.wilson@gmail.com>
2022-05-25 14:48:23 -0600

    through app-6

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// import logo from './logo.svg';
+import "./App.css";
+import Todo from "./Todo";
 
-function App() {
+const App = () => {
+  const [userInput, setUserInput] = useState("");
+  const [list, setList] = useState([]);
+
+  const changeHandler = (value) => {
+    setUserInput(value.target.value);
+  };
+
+  const clickHandler = () => {
+    setList([...list, userInput]);
+  };
+
+  let displayList = list.map((element, index) => {
+    return <Todo key={index} task={element} />;
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input onChange={changeHandler} />
+        <button onClick={clickHandler}>Add</button>
+      </div>
+      <div>
+        <ol>{displayList}</ol>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
